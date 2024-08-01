77d88a520616a57729fd8eff669db44b838761b5
Zhang <2751628691@qq.com>
2024-08-01 10:41:51 +0800

    build: 项目初始化

---
+++ src/main/java/com/zjz/zoj/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.zjz.zoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zjz.zoj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
